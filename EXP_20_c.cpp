08b15e07428e057aef8787da278b496877433427
Piyush <118040098+piyubebbe@users.noreply.github.com>
2025-10-04 21:09:44 +0530

    Add files via upload

---
+++ EXP_20_c.cpp
@@ -0,0 +1,53 @@
+//Piyush Pawar
+//24070123145
+//B3
+
+#include <iostream>
+using namespace std;
+
+void swap(int*a,int*b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+int main()
+{
+    int elements;
+    cout << "no. of elements :";
+    cin >> elements;
+    int array[elements];
+    cout << "Enter elements:";
+    for (int i = 0; i < elements; i++) 
+        cin >> array[i];
+    
+    for (int i = 0; i < elements; i++) 
+        cout << array[i] << " ";
+    
+    int n = 0;
+    while (n != elements) 
+    {
+        for (int i = 0; i < elements - n; i++) 
+            if (array[i] > array[i + 1]) 
+                swap(&array[i], &array[i + 1]);
+            
+        n++;
+    }
+
+    cout << "\nSorted array is:" << endl;
+    for (int i = 0; i < elements; i++) 
+        cout << array[i] << " ";
+    
+    return 0;
+}
+
+/*
+Output:
+no. of elements: 6 
+Enter elements: 
+6 5 1 2 13 8
+Sorted array is:
+1 2 5 6 8 13 
+*/
